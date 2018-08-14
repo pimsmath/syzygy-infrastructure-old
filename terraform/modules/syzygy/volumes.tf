9ae1d19e0d692226fd7d15d6d12d7d004a07c8e9
Ian Allison <iana@pims.math.ca>
2018-08-14 14:58:05 -0700

    Matching up syzygy and hub
    Signed-off-by: Ian Allison <iana@pims.math.ca>

--- terraform/modules/syzygy/volumes.tf
+++ terraform/modules/syzygy/volumes.tf
@@ -1,3 +1,11 @@
+# Determine the volume UUIDs, whether if existing ones were supplied
+# or if new ones were created.
+locals {
+  vol_id_1 = "${length(var.existing_volumes) == 0 ?
+    element(concat(openstack_blockstorage_volume_v2.homedir.*.id, list("")), 0) :
+    element(concat(var.existing_volumes, list("")), 0)
+  }"
+}
 resource "openstack_blockstorage_volume_v2" "homedir" {
   count = 1
   name  = "${format("%s-homedir-%02d", var.name, count.index+1)}"
@@ -8,4 +16,3 @@ resource "openstack_compute_volume_attach_v2" "homedir_1" {
   instance_id = "${openstack_compute_instance_v2.instance.id}"
   volume_id   = "${openstack_blockstorage_volume_v2.homedir.0.id}"
 }
-
